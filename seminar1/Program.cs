fcdc61e337af5b8e421e4de218ccd6edb6590bb3
wsvetkin <wsvetkin@gmail.com>
2023-03-23 21:03:28 +0300

    создали новый проект квадрат числа

---
+++ seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите чило: ");
+string strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);
+int sqr  = num * num;
+
+// if sqr < 0 {
+//    sqr*=-1
+//}
+Console.WriteLine($"Квадрат будет равен:  = {sqr}");
+// Console.WriteLine(sqr);
+
+//Console.WriteLine("Hello, World!");
